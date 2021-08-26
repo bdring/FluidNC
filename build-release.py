77bd0619bc7b4a2a9c53500ed0cc029f8dbc59ec
Mitch Bradley <wmb@firmworks.com>
2021-08-26 06:44:30 -1000

    Cleanup and rename install.bat to install-win.bat

--- build-release.py
+++ build-release.py
@@ -10,12 +10,48 @@
 # see the details of an errored build, include -v on the command line.
 
 from shutil import copy
-from builder import buildEnv, buildFs
 from zipfile import ZipFile
 import subprocess, os, sys
 import urllib.request
 
 verbose = '-v' in sys.argv
+
+environ = dict(os.environ)
+
+def buildEnv(pioEnv, verbose=True, extraArgs=None):
+    cmd = ['platformio','run', "-e", pioEnv]
+    if extraArgs:
+        cmd.append(extraArgs)
+    displayName = pioEnv
+    print('Building firmware for ' + displayName)
+    if verbose:
+        app = subprocess.Popen(cmd, env=environ)
+    else:
+        app = subprocess.Popen(cmd, env=environ, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1)
+        for line in app.stdout:
+            line = line.decode('utf8')
+            if "Took" in line or 'Uploading' in line or ("error" in line.lower() and "Compiling" not in line):
+                print(line, end='')
+    app.wait()
+    print()
+    return app.returncode
+
+def buildFs(pioEnv, verbose=True, extraArgs=None):
+    cmd = ['platformio','run', '-e', pioEnv, '-t', 'buildfs']
+    if extraArgs:
+        cmd.append(extraArgs)
+    print('Building file system for ' + pioEnv)
+    if verbose:
+        app = subprocess.Popen(cmd, env=environ)
+    else:
+        app = subprocess.Popen(cmd, env=environ, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, bufsize=1)
+        for line in app.stdout:
+            line = line.decode('utf8')
+            if "Took" in line or 'Uploading' in line or ("error" in line.lower() and "Compiling" not in line):
+                print(line, end='')
+    app.wait()
+    print()
+    return app.returncode
 
 tag = (
     subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"])
@@ -32,6 +68,8 @@ zipFileName = os.path.join(relPath, 'fluidnc-' + tag + '.zip')
 
 with ZipFile(zipFileName, 'w') as zipObj:
     name = 'HOWTO-INSTALL.txt'
+    zipObj.write(os.path.join(sharedPath, name), name)
+    name = 'README-ESPTOOL.txt'
     zipObj.write(os.path.join(sharedPath, name), name)
 
     numErrors = 0
@@ -54,7 +92,7 @@ with ZipFile(zipFileName, 'w') as zipObj:
                 objPath = os.path.join(pioPath, envName)
                 for obj in ['firmware.bin','partitions.bin']:
                     zipObj.write(os.path.join(objPath, obj), os.path.join(envName, obj))
-                for obj in ['install.bat','install-linux.sh','install-macos.sh']:
+                for obj in ['install-win.bat','install-linux.sh','install-macos.sh']:
                     zipObj.write(os.path.join(sharedPath, obj), os.path.join(envName, obj))
         EsptoolVersion = 'v3.1'
         EspRepo = 'https://github.com/espressif/esptool/releases/download/' + EsptoolVersion + '/'
