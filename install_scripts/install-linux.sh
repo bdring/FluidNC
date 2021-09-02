1df483a39429b88e2cfd01d05bcb9fe7f81e57fb
Mitch Bradley <wmb@firmworks.com>
2021-09-02 08:36:08 -1000

    Installer was missing bootloader images (#12)
    .. so it would not work on a freshly-erased ESP32

--- install_scripts/install-linux.sh
+++ install_scripts/install-linux.sh
@@ -4,10 +4,11 @@ EsptoolPath=../linux-amd64/esptool
 
 SetupArgs="--chip esp32 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect"
 
-Bootloader="0x1000 bootloader_qio_80m.bin"
+Bootloader="0x1000 ../common/bootloader_dio_80m.bin"
+Bootapp="0xe000 ../common/boot_app0.bin"
 Firmware="0x10000 firmware.bin"
 Partitions="0x8000 partitions.bin"
 LocalFS="0x3d0000 ../common/spiffs.bin"
 
-echo $EsptoolPath $SetupArgs $Firmware $Partitions $LocalFs
-$EsptoolPath $SetupArgs $Firmware $Partitions $LocalFs
+echo $EsptoolPath $SetupArgs $Bootloader $Bootapp $Firmware $Partitions $LocalFs
+$EsptoolPath $SetupArgs $Bootloader $Bootapp $Firmware $Partitions $LocalFs
