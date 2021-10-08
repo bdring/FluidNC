6f53e16f0962947765ef64b7723a949e6efafb2e
bdring <barton.dring@gmail.com>
2021-10-08 16:09:31 -0500

    Better defaults

--- FluidNC/src/Machine/Axis.h
+++ FluidNC/src/Machine/Axis.h
@@ -29,10 +29,10 @@ namespace Machine {
         Motor*  _motors[MAX_MOTORS_PER_AXIS];
         Homing* _homing = nullptr;
 
-        float _stepsPerMm   = 320.0f;
+        float _stepsPerMm   = 80.0f;
         float _maxRate      = 1000.0f;
         float _acceleration = 25.0f;
-        float _maxTravel    = 200.0f;
+        float _maxTravel    = 1000.0f;
         bool  _softLimits   = false;
 
         // Configuration system helpers:
