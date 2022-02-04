9f9d2e8407fb565da0a152b3ad939f43de671625
Owen Carter <owen.carter@gmail.com>
2022-02-04 21:21:00 +0100

    check for correct path on older py3 installs

--- install_scripts/linux-python3/tools.sh
+++ install_scripts/linux-python3/tools.sh
@@ -1,12 +1,24 @@
 # Subroutines to call esptool with common arguments
 
+# Ensure local binary path is in $PATH (pip3 default target path)
+echo "$PATH" | grep '$HOME\/\.local\/bin' 2>&1 >/dev/null
+if test "$?" != "0"; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+
 # Install esptool.py if needed
-which esptool.py
+which esptool.py 2>&1 >/dev/null
 if test "$?" != "0"; then
     echo esptool.py not found, attempting to install
     python3 -m pip install esptool
     if test "$?" != "0"; then
         echo esptool.py install failed
+        exit 1
+    fi
+    which esptool.py 2>&1 >/dev/null
+    if test "$?" != "0"; then
+        echo esptool.py claims to have installed successfully, but cannot be found in PATH
+        echo PATH= $PATH
         exit 1
     fi
 fi
