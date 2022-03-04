cdb1f62827f1bc4eb0dcd808b7b6c7a72f1ba702
Stefan de Bruijn <stefan@nubilosoft.com>
2022-03-04 15:42:31 +0100

    Fixed a few bugs in the pin extender

--- FluidNC/src/Machine/LimitPin.cpp
+++ FluidNC/src/Machine/LimitPin.cpp
@@ -46,6 +46,7 @@ namespace Machine {
 
     void IRAM_ATTR LimitPin::handleISR() {
         read();
+
         if (sys.state != State::Alarm && sys.state != State::ConfigAlarm && sys.state != State::Homing) {
             if (_pHardLimits && rtAlarm == ExecAlarm::None) {
 #if 0
@@ -58,7 +59,7 @@ namespace Machine {
                 }
 #endif
 
-                // log_debug("Hard limits");  // This might not work from ISR context
+                // log_debug("Hard limits");        // This might not work from ISR context
                 mc_reset();                      // Initiate system kill.
                 rtAlarm = ExecAlarm::HardLimit;  // Indicate hard limit critical event
             }
@@ -88,6 +89,7 @@ namespace Machine {
         if (_pin.undefined()) {
             return;
         }
+
         set_bitnum(Axes::limitMask, _axis);
         _pin.report(_legend.c_str());
         auto attr = Pin::Attr::Input | Pin::Attr::ISR;
