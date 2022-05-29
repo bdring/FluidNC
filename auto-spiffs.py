6787ae686eb565a453de7705090535bf1de6c3b5
Avataar120 <dric.vivien@gmail.com>
2022-05-29 23:24:13 +0200

    Fix issue that was preventing to compile (subprocess output)

--- auto-spiffs.py
+++ auto-spiffs.py
@@ -1,5 +1,5 @@
 import subprocess
 
-subprocess.run (["copy", "example_configs\\4_AvaShield_7.3_K40.yaml", "spiffs\\output\\config.yaml"], shell=True)
-subprocess.run (["spiffs\\mkspiffs", "-c", "spiffs\\output", "spiffs\\spiffs.bin", "-s",  "196608"])
-subprocess.run (["copy", "spiffs\\spiffs.bin", "install_scripts\\win64\\wifi"], shell=True)
+subprocess.check_call (["copy", "example_configs\\4_AvaShield_7.3_K40.yaml", "spiffs\\output\\config.yaml"], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.check_call (["spiffs\\mkspiffs", "-c", "spiffs\\output", "spiffs\\spiffs.bin", "-s",  "196608"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+subprocess.check_call (["copy", "spiffs\\spiffs.bin", "install_scripts\\win64\\wifi"], shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
