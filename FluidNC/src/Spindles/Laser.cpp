b59acd58ee03460798afb87d79523bb8381eedbe
Mitch Bradley <wmb@firmworks.com>
2022-07-11 13:22:03 -1000

    Converted the PwmPin API to a C++ class (#519)
    * Converted the PwmPin API to a C++ class
    
    * Use ledc_timer_config() instead of doing the work manually

--- FluidNC/src/Spindles/Laser.cpp
+++ FluidNC/src/Spindles/Laser.cpp
@@ -7,7 +7,7 @@
 */
 
 #include "Laser.h"
-#include "../Pins/LedcPin.h"
+#include "Driver/PwmPin.h"  // pwmInit(), etc.
 
 #include "../Machine/MachineConfig.h"
 
@@ -19,8 +19,8 @@ namespace Spindles {
     }
 
     void Laser::config_message() {
-        log_info(name() << " Spindle Ena:" << _enable_pin.name() << " Out:" << _output_pin.name() << " Freq:" << _pwm_freq
-                        << "Hz Res:" << _pwm_precision << "bits Laser mode:On");
+        log_info(name() << " Spindle Ena:" << _enable_pin.name() << " Out:" << _output_pin.name() << " Freq:" << _pwm->frequency()
+                        << "Hz Period:" << _pwm->period() << " Laser mode:On");
     }
 
     // Get the GPIO from the machine definition
@@ -30,15 +30,12 @@ namespace Spindles {
 
         is_reversable = false;
 
-        _pwm_precision = ledc_calc_pwm_precision(_pwm_freq);  // determine the best precision
-        _pwm_period    = (1 << _pwm_precision);
-
         if (_speeds.size() == 0) {
             // The default speed map for a Laser is linear from 0=0% to 255=100%
             linearSpeeds(255, 100.0f);
         }
 
-        setupSpeeds(_pwm_period);
+        setupSpeeds(_pwm->period());
     }
 
     // Configuration registration
