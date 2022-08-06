3e16d5184387ce06e8373a80a7392e14b80a5adf
Mitch Bradley <wmb@firmworks.com>
2022-08-06 11:48:34 -1000

    Suppress error messages from ESP_IDF, fixed littlefs unmount/format

--- FluidNC/esp32/littlefs.cpp
+++ FluidNC/esp32/littlefs.cpp
@@ -1,6 +1,8 @@
 #include "esp_partition.h"
 #include "esp_littlefs.h"
+#include "esp_log.h"
 #include "wdt.h"
+#include "Driver/localfs.h"
 #include "src/Logging.h"
 
 // Remember the partition label of the littlefs filesystem -
@@ -10,28 +12,17 @@ const char* littlefs_label;
 bool littlefs_format(const char* partition_label) {
     esp_err_t err;
     disable_core0_WDT();
-    if (partition_label) {
-        log_debug("esp_littlefs_format " << partition_label);
-        err = esp_littlefs_format(partition_label);
-    } else {
-        err = esp_littlefs_format("littlefs");
-        if (err) {
-            auto part = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_SPIFFS, NULL);
-
-            const char* label = part->label;
-            log_info("Trying partition named " << label);
-            err = esp_littlefs_format(label);
-        }
-    }
+    err = esp_littlefs_format(partition_label);
     enable_core0_WDT();
     if (err) {
-        log_info("LittleFS format failed - " << esp_err_to_name(err));
+        log_debug("LittleFS format in " << partition_label << " partition failed: " << esp_err_to_name(err));
         return true;
     }
     return false;
 }
 
 bool littlefs_mount(const char* label, bool format) {
+    esp_log_level_set("esp_littlefs", ESP_LOG_NONE);
     esp_vfs_littlefs_conf_t conf = { .base_path = "/littlefs", .partition_label = label, .format_if_mount_failed = format };
 
     esp_err_t err = esp_vfs_littlefs_register(&conf);
@@ -42,5 +33,5 @@ bool littlefs_mount(const char* label, bool format) {
     return err;
 }
 void littlefs_unmount() {
-    esp_vfs_littlefs_unregister("littlefs");
+    esp_vfs_littlefs_unregister(littlefs_label);
 }
