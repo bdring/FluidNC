5da5d2c76a674a55045e700099ae0dc822d2ab1c
grittershub <31842574+grittershub@users.noreply.github.com>
2022-08-22 21:29:19 +1000

    remove extraneous TrinamicBase

--- FluidNC/src/Motors/TrinamicBase.h
+++ FluidNC/src/Motors/TrinamicBase.h
@@ -60,7 +60,7 @@ namespace MotorDrivers {
         bool         set_homing_mode(bool isHoming);
         virtual void set_registers(bool isHoming) {}
         bool         reportTest(uint8_t result);
-        void         TrinamicBase::reportCommsFailure(void);
+        void         reportCommsFailure(void);
         bool         startDisable(bool disable);
         virtual void config_motor();
 
