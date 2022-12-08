f79cf63fe68a821b2a34d4a19c59c946ea3c984d
bdring <barton.dring@gmail.com>
2022-12-08 12:58:42 -0600

    Adding new class

---
+++ FluidNC/src/Motors/TMC5160ProDriver.h
@@ -0,0 +1,46 @@
+// Copyright (c) 2020 -	Bart Dring
+// Use of this source code is governed by a GPLv3 license that can be found in the LICENSE file.
+
+#pragma once
+
+#include "TrinamicSpiDriver.h"
+#include "../Pin.h"
+#include "../PinMapper.h"
+
+#include <cstdint>
+
+const float TMC5160_RSENSE_DEFAULT = 0.075f;
+
+namespace MotorDrivers {
+
+    class TMC5160ProDriver : public TrinamicSpiDriver {
+    public:
+        // Overrides for inherited methods
+        void init() override;
+        void set_disable(bool disable);
+        void config_motor() override;
+        void debug_message() override;
+        void validate() const override { StandardStepper::validate(); }
+
+        void group(Configuration::HandlerBase& handler) override {
+            TrinamicSpiDriver::group(handler);
+            //handler.item("tpfd", _tpfd, 0, 15);
+            handler.item("CHOPCONF", CHOPCONF, 0, 0xFFFFFFFF);
+        }
+
+        // Name of the configurable. Must match the name registered in the cpp file.
+        const char* name() const override { return "tmc_5160Pro"; }
+
+    private:
+        TMC5160Stepper* tmc5160 = nullptr;
+
+        //uint8_t _tpfd = 4;
+
+        uint32_t CHOPCONF = 0;
+
+        bool test();
+        void set_registers(bool isHoming);
+        void trinamic_test_response();
+        void trinamic_stepper_enable(bool enable);
+    };
+}
