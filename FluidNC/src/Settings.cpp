2df7b5807cbf9b909eaa33a581b05cc5dba4ae04
bdring <barton.dring@gmail.com>
2022-12-26 13:52:25 -0600

    Remove log_info used for development

--- FluidNC/src/Settings.cpp
+++ FluidNC/src/Settings.cpp
@@ -120,7 +120,6 @@ void IntSetting::setDefault() {
 }
 
 Error IntSetting::setStringValue(char* s) {
-    log_info("IntSetting::setStringValue");
     s         = trim(s);
     Error err = check(s);
     if (err != Error::Ok) {
