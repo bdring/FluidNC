7d401d919edceb11ca1bba4eed350df7a99949c1
bdring <barton.dring@gmail.com>
2023-01-11 13:23:30 -0600

    Add TMC2160

---
+++ FluidNC/src/Motors/TMC2160Driver.cpp
@@ -0,0 +1,14 @@
+// Copyright (c) 2020 -	Bart Dring
+// Use of this source code is governed by a GPLv3 license that can be found in the LICENSE file.
+
+#include "TMC2160Driver.h"
+#include "../Machine/MachineConfig.h"
+#include <atomic>
+
+namespace MotorDrivers {
+
+    // Configuration registration
+    namespace {
+        MotorFactory::InstanceBuilder<TMC2160Driver> registration("tmc_2160");
+    }
+}
