53b3e01f76f68c41c83b1a8e5813adee1fe994db
gos2two <sergio.gosalvez@imperial.ac.uk>
2023-01-22 23:56:23 +0100

    Include on latest FluidNC Ben's analog read (32)

--- FluidNC/src/WebUI/WebServer.cpp
+++ FluidNC/src/WebUI/WebServer.cpp
@@ -1085,6 +1085,7 @@ namespace WebUI {
 
     void Web_Server::handle() {
         static uint32_t start_time = millis();
+        static uint32_t paige_potent_start_time = millis();
         if (WiFi.getMode() == WIFI_AP) {
             dnsServer.processNextRequest();
         }
@@ -1102,6 +1103,14 @@ namespace WebUI {
             }
 
             start_time = millis();
+        }
+        if ((millis() - paige_potent_start_time) > 500 && _socket_server) {
+            int paige_raw_pot_value = analogRead(32);
+            int paige_mapped_pot_value = map(paige_raw_pot_value, 0, 4095, 0, 100);
+            String s = "PAIGE_POTENT_VALUE:";
+            s += String(paige_mapped_pot_value);
+            _socket_server->broadcastTXT(s);
+            paige_potent_start_time = millis();
         }
     }
 
