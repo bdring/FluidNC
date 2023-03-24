864116d869eada4eaa8329c3a7986fb9d72f2cf9
bdring <barton.dring@gmail.com>
2023-03-24 14:42:15 -0500

    Set I_scale_analog=false
    This prevents people from using the pot from changing the scaling.
    
    We use amps in the config, so it does not makes sense to allow this.
    
    5160 does not have this feature.

--- FluidNC/src/Motors/TMC2209Driver.cpp
+++ FluidNC/src/Motors/TMC2209Driver.cpp
@@ -42,6 +42,7 @@ namespace MotorDrivers {
         // but the TMCStepper library expresses run current as (uint16_t) mA
         // and hold current as (float) fraction of run current.
         uint16_t run_i = (uint16_t)(_run_current * 1000.0);
+        tmc2209->I_scale_analog(false);  // do not scale via pot
         tmc2209->rms_current(run_i, TrinamicBase::holdPercent());
 
         // The TMCStepper library uses the value 0 to mean 1x microstepping
