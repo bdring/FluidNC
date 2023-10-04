a99d5605dde3ff587b66a9ff62942a5b3719a041
Avataar120 <dric.vivien@gmail.com>
2023-10-04 20:16:35 +0200

    Try to add answer to Lightburn read settings

--- FluidNC/src/SettingsDefinitions.h
+++ FluidNC/src/SettingsDefinitions.h
@@ -8,7 +8,15 @@ extern StringSetting* build_info;
 
 extern StringSetting* start_message;
 
-extern IntSetting* status_mask;
+extern IntSetting*    status_mask;
+extern StringSetting* Xspeed;
+extern StringSetting* Yspeed;
+extern StringSetting* Xaccel;
+extern StringSetting* Yaccel;
+extern StringSetting* X120;
+extern StringSetting* Y121;
+extern StringSetting* X130;
+extern StringSetting* Y131;
 
 extern IntSetting* sd_fallback_cs;
 
