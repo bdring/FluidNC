957bd08766fec431d8231d828aca837a26bc9667
azamat-bagatov <30553400+azamat-bagatov@users.noreply.github.com>
2023-11-21 14:49:53 +0200

    Build, install scripts and coredump partition disable

--- install_scripts/win64/install-fs.bat
+++ install_scripts/win64/install-fs.bat
@@ -1,19 +1,18 @@
 @echo off
 
-call checksecurity.bat
-if not %ErrorLevel% equ 0 (
-   exit /b 1
-)
 
 set EsptoolPath=win64\esptool.exe
 
-set BaseArgs=--chip esp32 --baud 921600
+set BaseArgs=--chip esp32s3 --baud 921600
 set SetupArgs=--before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect
 
-set LocalFS=0x3d0000 wifi\littlefs.bin
+set LocalFS=0x3d0000 wifi_s3\littlefs.bin
 
 echo %EsptoolPath% %BaseArgs% %SetupArgs% %LocalFS%
 %EsptoolPath% %BaseArgs% %SetupArgs% %LocalFS%
+
+
+exit /b 0
 
 echo Starting fluidterm
 win64\fluidterm.exe
