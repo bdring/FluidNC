3e94650100ded590d832089beedc60791a95fde1
Mitch Bradley <wmb@firmworks.com>
2023-12-10 14:29:48 -1000

    Fix unitialized UART config that caused inaccurate high baud rates

--- FluidNC/src/Uart.cpp
+++ FluidNC/src/Uart.cpp
@@ -20,6 +20,7 @@ static void uart_driver_n_install(void* arg) {
 void Uart::begin(unsigned long baud, UartData dataBits, UartStop stopBits, UartParity parity) {
     //    uart_driver_delete(_uart_num);
     uart_config_t conf;
+    conf.source_clk          = UART_SCLK_APB;
     conf.baud_rate           = baud;
     conf.data_bits           = uart_word_length_t(_dataBits);
     conf.parity              = uart_parity_t(_parity);
