b65a836d224149bbeda769ab31ee945b86bbb759
matt <10769515+whosmatt@users.noreply.github.com>
2024-01-28 14:34:24 +0100

    added union structs for control and status words

--- FluidNC/src/Spindles/DanfossSpindle.cpp
+++ FluidNC/src/Spindles/DanfossSpindle.cpp
@@ -48,8 +48,8 @@ namespace Spindles {
 
         // Speed Command: 4000 HEX = 100% speed
         // 40% of 4000 HEX = 1999 HEX(reversed)
-        data.msg[10]  = dev_speed >> 8;
-        data.msg[9] = dev_speed & 0xFF;
+        data.msg[10] = dev_speed >> 8;
+        data.msg[9]  = dev_speed & 0xFF;
     }
 
     void DanfossVLT2800::direction_command(SpindleState mode, ModbusCommand& data) {
@@ -58,7 +58,7 @@ namespace Spindles {
 #endif
         // fluidNC does not currently allow defining a VFDSpindle without direction command and data is always sent
         // send valid request to avoid error
-        get_current_speed(data); 
+        get_status_ok(data);
     }
 
     VFD::response_parser DanfossVLT2800::get_current_speed(ModbusCommand& data) {
@@ -77,7 +77,7 @@ namespace Spindles {
             // const uint8_t function = response[1]
             // const uint8_t response_byte_count = response[2]
 
-            uint16_t freq = (uint16_t(response[3]) << 8) | uint16_t(response[4]);
+            uint16_t freq        = (uint16_t(response[3]) << 8) | uint16_t(response[4]);
             vfd->_sync_dev_speed = freq;
             return true;
         };
@@ -95,50 +95,28 @@ namespace Spindles {
         data.msg[5] = 0x10;  // Read 16 Bits
 
         return [](const uint8_t* response, Spindles::VFD* vfd) -> bool {
-            // const uint8_t slave_addr = response[0]
-            // const uint8_t function = response[1]
-            // const uint8_t response_byte_count = response[2]
-            // const uint8_t data_1 = response[3];  // Data (Coils 40-33)
-            // const uint8_t data_2 = response[4];  // Data (Coils 48-41)
-
-            const uint16_t statusword = int16_t(response[3]) | uint16_t(response[4] << 8);
-            SpindleStatus  status;
-
-            status.control_ready = statusword >> 0 & 1;
-            status.drive_ready   = statusword >> 1 & 1;
-            status.coasting_stop = statusword >> 2 & 1;
-            status.trip          = statusword >> 3 & 1;
-
-            status.trip_lock      = statusword >> 6 & 1;
-            status.warning        = statusword >> 7 & 1;
-            status.speed_status   = statusword >> 8 & 1;
-            status.local_control  = statusword >> 9 & 1;
-            status.freq_range_err = statusword >> 10 & 1;
-            status.motor_running  = statusword >> 11 & 1;
-
-            status.voltage_warn  = statusword >> 13 & 1;
-            status.current_limit = statusword >> 14 & 1;
-            status.thermal_warn  = statusword >> 15 & 1;
+            SpindleStatus status;
+            status.statusWord = int16_t(response[3]) | uint16_t(response[4] << 8);  // See DanfossSpindle.h for structure
 
 #ifdef DEBUG_VFD
             // Source: https://files.danfoss.com/download/Drives/MG10S202.pdf [Page 22]
-            log_debug("Control ready:" << status.control_ready);
-            log_debug("Drive ready:" << status.drive_ready);
-            log_debug("Coasting stop:" << status.warning);
-            log_debug("Trip status:" << status.trip);
+            log_debug("Control ready:" << status.flags.control_ready);
+            log_debug("Drive ready:" << status.flags.drive_ready);
+            log_debug("Coasting stop:" << status.flags.warning);
+            log_debug("Trip status:" << status.flags.trip);
 
-            log_debug("Trip lock:" << status.trip_lock);
-            log_debug("No warning/warning:" << status.warning);
+            log_debug("Trip lock:" << status.flags.trip_lock);
+            log_debug("No warning/warning:" << status.flags.warning);
 
-            log_debug("Speed == ref:" << status.speed_status);
-            log_debug("Local operation/serial communication control:" << status.local_control);
-            log_debug("Outside frequency range:" << status.freq_range_err);
-            log_debug("Motor running:" << status.motor_running);
+            log_debug("Speed == ref:" << status.flags.speed_status);
+            log_debug("Local operation/serial communication control:" << status.flags.local_control);
+            log_debug("Outside frequency range:" << status.flags.freq_range_err);
+            log_debug("Motor running:" << status.flags.motor_running);
 
-            log_debug("Not used:" << status.voltage_warn);
-            log_debug("Voltage warn:" << status.voltage_warn);
-            log_debug("Current limit:" << status.current_limit);
-            log_debug("Thermal warn:" << status.thermal_warn);
+            log_debug("Not used:" << status.flags.voltage_warn);
+            log_debug("Voltage warn:" << status.flags.voltage_warn);
+            log_debug("Current limit:" << status.flags.current_limit);
+            log_debug("Thermal warn:" << status.flags.thermal_warn);
 #endif
 
             log_error("TODO: actually check status bits and output potential errors");
