d9ecd9e042d6e4717ac734a29cd2d2b457c4dde8
BarbourSmith <BarbourSmith@users.noreply.github.com>
2024-04-02 15:44:48 -0700

    Update build-release.py

--- build-release.py
+++ build-release.py
@@ -12,8 +12,8 @@ verbose = '-v' in sys.argv
 environ = dict(os.environ)
 
 platformio = r"C:\Users\bar\.platformio\penv\Scripts\platformio.exe"
-version = "0.62.2"
-os.chdir(os.path.dirname(os.path.realpath(r"C:\Users\bar\OneDrive\Documents\GitHub\FluidNC\.pio")))
+version = "0.66.7"
+os.chdir(os.path.dirname(os.path.realpath(r"C:\Users\bar\Documents\GitHub\FluidNC\.pio")))
 #change path to the project folder (the folder with platformio.ini)
 tag = "maslow4-"+version
 sharedPath = 'install_scripts'
