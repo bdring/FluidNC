40fc47405254bd00f2f786574ef2b7b8060edabc
BarbourSmith <BarbourSmith@users.noreply.github.com>
2024-07-31 10:11:47 -0700

    Disable the over current warning panic

--- FluidNC/src/Maslow/Maslow.cpp
+++ FluidNC/src/Maslow/Maslow.cpp
@@ -600,8 +600,8 @@ void Maslow_::safety_control() {
             panicCounter[i]++;
             if (panicCounter[i] > tresholdHitsBeforePanic) {
                 if(sys.state() == State::Jog || sys.state() == State::Cycle){
-                    log_error("Motor current on " << axis_id_to_label(i).c_str() << " axis exceeded threshold of " << 4000);
-                    Maslow.panic();
+                    log_warn("Motor current on " << axis_id_to_label(i).c_str() << " axis exceeded threshold of " << 4000);
+                    // Maslow.panic();
                 }
                 tick[i] = true;
             }
