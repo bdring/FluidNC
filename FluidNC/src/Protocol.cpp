5702c1a010fbcad079989e3c94e72a55a587fd59
bdring <barton.dring@gmail.com>
2024-12-27 08:08:57 -0600

    Remove end motion

--- FluidNC/src/Protocol.cpp
+++ FluidNC/src/Protocol.cpp
@@ -485,7 +485,7 @@ static void protocol_start_holding() {
     if (!(sys.suspend.bit.motionCancel || sys.suspend.bit.jogCancel)) {  // Block, if already holding.
         sys.step_control = {};
         if (!Stepper::update_plan_block_parameters()) {  // Notify stepper module to recompute for hold deceleration.
-            sys.step_control.endMotion = true;
+            //sys.step_control.endMotion = true;
         }
         sys.step_control.executeHold = true;  // Initiate suspend state with active flag.
     }
@@ -496,7 +496,7 @@ static void protocol_cancel_jogging() {
         sys.step_control = {};
         Stepper::update_plan_block_parameters();
         sys.step_control.executeHold = true;  // Initiate suspend state with active flag.
-        sys.suspend.bit.jogCancel = true;
+        sys.suspend.bit.jogCancel    = true;
     }
 }
 
