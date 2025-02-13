3b47e83549cd488df9d352cf5e0d7b82efbac777
Will Puckett <willpuckett@gmail.com>
2025-02-13 09:21:44 -0800

    enlarge partition to fully utilize disk

--- install_scripts/posix/full-install.sh
+++ install_scripts/posix/full-install.sh
@@ -5,9 +5,10 @@ LocalFS="0x5F0000 wifi_s3/littlefs.bin"
 
 if ! . ./tools.sh; then exit 1; fi
 
-esptool_erase
+# check_security runs in esptool_erase & install
+# if ! check_security; then exit 1; fi
 
-if ! check_security; then exit 1; fi
+esptool_erase
 
 esptool_write $LocalFS
 
