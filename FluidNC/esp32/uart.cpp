9a3e44c747e117c0912582311a942c34b9522e61
Mitch Bradley <wmb@firmworks.com>
2025-03-15 10:15:58 -1000

    Local copy of ESP_IDF UART driver

--- FluidNC/esp32/uart.cpp
+++ FluidNC/esp32/uart.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <Driver/fluidnc_uart.h>
 
-#include <driver/uart.h>
+// #include <driver/uart.h>
+#include "fnc_idf_uart.h"
 #include <esp_ipc.h>
 #include "hal/uart_hal.h"
 
 static void uart_driver_n_install(void* arg) {
-    uart_driver_install((uart_port_t)arg, 256, 0, 0, NULL, ESP_INTR_FLAG_IRAM);
+    uart_port_t port = (uart_port_t)arg;
+    if (port) {
+        fnc_uart_driver_install(port, 256, 0, 0, NULL, ESP_INTR_FLAG_IRAM);
+    } else {
+        uart_driver_install(port, 256, 0, 0, NULL, ESP_INTR_FLAG_IRAM);
+    }
 }
 
 void uart_init(int uart_num) {
@@ -24,24 +30,47 @@ void uart_mode(int uart_num, unsigned long baud, UartData dataBits, UartParity p
     conf.stop_bits           = uart_stop_bits_t(stopBits);
     conf.flow_ctrl           = UART_HW_FLOWCTRL_DISABLE;
     conf.rx_flow_ctrl_thresh = 0;
-    uart_param_config(uart_port_t(uart_num), &conf);
+
+    uart_port_t port = (uart_port_t)uart_num;
+    if (port) {
+        fnc_uart_param_config(port, &conf);
+    } else {
+        uart_param_config(port, &conf);
+    }
 }
 
 bool uart_half_duplex(int uart_num) {
-    return uart_set_mode(uart_port_t(uart_num), UART_MODE_RS485_HALF_DUPLEX) != ESP_OK;
+    uart_port_t port = (uart_port_t)uart_num;
+    if (port) {
+        return fnc_uart_set_mode(port, UART_MODE_RS485_HALF_DUPLEX) != ESP_OK;
+    } else {
+        return uart_set_mode(port, UART_MODE_RS485_HALF_DUPLEX) != ESP_OK;
+    }
 }
 
 int uart_read(int uart_num, uint8_t* buf, int len, int timeout_ms) {
-    return uart_read_bytes(uart_port_t(uart_num), buf, len, timeout_ms);
+    uart_port_t port = (uart_port_t)uart_num;
+    if (port) {
+        return fnc_uart_read_bytes(port, buf, len, timeout_ms);
+    } else {
+        return uart_read_bytes(port, buf, len, timeout_ms);
+    }
 }
 int uart_write(int uart_num, const uint8_t* buf, int len) {
-    return uart_write_bytes(uart_port_t(uart_num), buf, len);
+    uart_port_t port = (uart_port_t)uart_num;
+    if (port) {
+        return fnc_uart_write_bytes(port, buf, len);
+    } else {
+        return uart_write_bytes(port, buf, len);
+    }
 }
 void uart_xon(int uart_num) {
-    uart_ll_force_xon(uart_port_t(uart_num));
+    uart_port_t port = (uart_port_t)uart_num;
+    uart_ll_force_xon(port);
 }
 void uart_xoff(int uart_num) {
-    uart_ll_force_xoff(uart_port_t(uart_num));
+    uart_port_t port = (uart_port_t)uart_num;
+    uart_ll_force_xoff(port);
 }
 void uart_sw_flow_control(int uart_num, bool on, int xon_threshold, int xoff_threshold) {
     if (xon_threshold <= 0) {
@@ -50,19 +79,44 @@ void uart_sw_flow_control(int uart_num, bool on, int xon_threshold, int xoff_thr
     if (xoff_threshold <= 0) {
         xoff_threshold = 127;
     }
-    uart_set_sw_flow_ctrl(uart_port_t(uart_num), on, xon_threshold, xoff_threshold);
+    uart_port_t port = (uart_port_t)uart_num;
+    if (port) {
+        fnc_uart_set_sw_flow_ctrl(port, on, xon_threshold, xoff_threshold);
+    } else {
+        uart_set_sw_flow_ctrl(port, on, xon_threshold, xoff_threshold);
+    }
 }
 bool uart_pins(int uart_num, int tx_pin, int rx_pin, int rts_pin, int cts_pin) {
-    return uart_set_pin(uart_num, tx_pin, rx_pin, rts_pin, cts_pin) != ESP_OK;
+    uart_port_t port = (uart_port_t)uart_num;
+    if (port) {
+        return fnc_uart_set_pin(uart_num, tx_pin, rx_pin, rts_pin, cts_pin) != ESP_OK;
+    } else {
+        return uart_set_pin(uart_num, tx_pin, rx_pin, rts_pin, cts_pin) != ESP_OK;
+    }
 }
 int uart_buflen(int uart_num) {
-    size_t size;
-    uart_get_buffered_data_len(uart_port_t(uart_num), &size);
+    size_t      size;
+    uart_port_t port = (uart_port_t)uart_num;
+    if (port) {
+        fnc_uart_get_buffered_data_len(port, &size);
+    } else {
+        uart_get_buffered_data_len(port, &size);
+    }
     return size;
 }
 void uart_discard_input(int uart_num) {
-    uart_flush_input(uart_port_t(uart_num));
+    uart_port_t port = (uart_port_t)uart_num;
+    if (port) {
+        fnc_uart_flush_input(port);
+    } else {
+        uart_flush_input(port);
+    }
 }
 bool uart_wait_output(int uart_num, int timeout_ms) {
-    return uart_wait_tx_done(uart_port_t(uart_num), timeout_ms) != ESP_OK;
+    uart_port_t port = (uart_port_t)uart_num;
+    if (port) {
+        return fnc_uart_wait_tx_done(port, timeout_ms) != ESP_OK;
+    } else {
+        return uart_wait_tx_done(port, timeout_ms) != ESP_OK;
+    }
 }
