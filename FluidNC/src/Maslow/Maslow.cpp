b852c6f76227a324d714db91d26bac2fd961fcc1
BarbourSmith <BarbourSmith@users.noreply.github.com>
2025-07-01 16:02:04 -0700

    Clean up

--- FluidNC/src/Maslow/Maslow.cpp
+++ FluidNC/src/Maslow/Maslow.cpp
@@ -173,12 +173,6 @@ void Maslow_::update() {
             float blBeltLength = steps_to_mpos(get_axis_motor_steps(2), 2); // BL from C axis (axis 2)
             float brBeltLength = steps_to_mpos(get_axis_motor_steps(3), 3); // BR from D axis (axis 3)
             float zPosition = steps_to_mpos(get_axis_motor_steps(4), 4);    // Z from Z axis (axis 4)
-
-            if(random(100) == 1) {  // Randomly print the belt lengths for debugging
-                log_info("Belt lengths: TL=" << tlBeltLength << ", TR=" << trBeltLength
-                                             << ", BL=" << blBeltLength << ", BR=" << brBeltLength);
-                log_info("Z Position: " << zPosition);
-            }
             
             // Set individual belt targets using the computed positions
             axisTL.setTarget(tlBeltLength);
@@ -201,7 +195,7 @@ void Maslow_::update() {
                 }
             }
 
-            //We used to call Maslow.updatePositions() here, but now we use the axis system directly
+            //We used to call Maslow.setTargets() here, but now we use the axis system directly
 
             //This disables the belt motors until the user has completed calibration or apply tension and they have succeded
             if (calibration.currentState == READY_TO_CUT) {
@@ -335,8 +329,6 @@ void Maslow_::setTargets(float xTarget, float yTarget, float zTarget, bool tl, b
     if (br) {
         axisBR.setTarget(kinematics->computeBR(xTarget, yTarget, zTarget));
     }
-
-    log_info("TL Target: " << axisTL.getTarget() << " for X: " << xTarget << ", Y: " << yTarget << ", Z: " << zTarget);
 }
 
 // Get's the most recently set target position in X
