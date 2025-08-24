414e4d4c1aa3219a27545b3c63e9d362a68ed573
X <>
2025-08-24 15:26:19 -0700

    minor fix

--- FluidNC/src/ProcessSettings.cpp
+++ FluidNC/src/ProcessSettings.cpp
@@ -491,11 +491,11 @@ static Error home_w(const char* value, AuthenticationLevel auth_level, Channel& 
 static std::string limit_set(uint32_t mask) {
     const char* motor0AxisName = "xyzabcuvw";
     std::string s;
-    for (int axis = 0; axis < MAX_N_AXIS; axis++) {
+    for (int axis = 0; axis < MAX_N_AXIS - 1; axis++) {
         s += bitnum_is_true(mask, Machine::Axes::motor_bit(axis, 0)) ? char(motor0AxisName[axis]) : ' ';
     }
     const char* motor1AxisName = "XYZABCUVW";
-    for (int axis = 0; axis < MAX_N_AXIS; axis++) {
+    for (int axis = 0; axis < MAX_N_AXIS - 1; axis++) {
         s += bitnum_is_true(mask, Machine::Axes::motor_bit(axis, 1)) ? char(motor1AxisName[axis]) : ' ';
     }
     return s;
