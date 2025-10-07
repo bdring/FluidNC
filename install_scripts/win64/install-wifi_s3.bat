716cadc747e8c6087ac2d1bda175d2c826a0882c
Mitch Bradley <wmb@firmworks.com>
2025-10-07 06:51:15 -1000

    Added/fixed windows installer scripts for S3

--- install_scripts/win64/install-wifi_s3.bat
+++ install_scripts/win64/install-wifi_s3.bat
@@ -8,7 +8,7 @@ if not %ErrorLevel% equ 0 (
 set BuildType=wifi_s3
 set EsptoolPath=win64\esptool.exe
 
-set BaseArgs=--chip esp32 --baud 921600
+set BaseArgs=--chip esp32s3 --baud 921600
 set SetupArgs=--before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect
 
 set Bootloader=0x1000 %BuildType%\bootloader.bin
