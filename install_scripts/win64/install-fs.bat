fed70707b28ee78dd35c84252d41df75c20fee18
PhiJu5 <PhiJu5@users.noreply.github.com>
2025-10-08 09:22:21 +0200

    Remove esptool --chip option when not necessary in install_scripts directory
    From espressif esptool ESP32 and ESP32S3 'Basic Options":
    
    If no -c option or ESPTOOL_CHIP value is specified, esptool automatically detects the chip type when connecting.
    
    Binary image generation commands, such as elf2image or merge-bin, require the chip type to be specified.
    
    After a wifi_s3 build
    
    py generate_vcxproj.py
    pio run -e wifi_s3
    
    .pio\build\wifi_s3\.sconsign311.dblite
    constains expected --chip esp32s3 elf2image
    
    After a wifi build
    .pio\build\wifi\.sconsign311.dblite
    constains expected --chip esp32 elf2image

--- install_scripts/win64/install-fs.bat
+++ install_scripts/win64/install-fs.bat
@@ -7,7 +7,7 @@ if not %ErrorLevel% equ 0 (
 
 set EsptoolPath=win64\esptool.exe
 
-set BaseArgs=--chip esp32 --baud 921600
+set BaseArgs=--baud 921600
 set SetupArgs=--before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 80m --flash_size detect
 
 set LocalFS=0x3d0000 wifi\littlefs.bin
