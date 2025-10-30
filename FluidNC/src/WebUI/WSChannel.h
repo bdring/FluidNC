19caf03e1444040aa522f39dfdbd008ed520d345
Gerporgl <contributor@example.com>
2025-10-30 17:19:46 -0400

    Fixed sendPing, but send the same ping to all websocket clients (broadcast), need to test more see if that cause any adverse effect

--- FluidNC/src/WebUI/WSChannel.h
+++ FluidNC/src/WebUI/WSChannel.h
@@ -64,13 +64,12 @@ namespace WebUI {
 
     class WSChannels {
     private:
-        static std::map<uint32_t, WSChannel*>    _wsChannels;
-        static std::list<WSChannel*>             _webWsChannels;
-        static std::map<std::string, WSChannel*> _wsChannelsBySession;
-        static AsyncWebSocket*                   _server;
+        static std::map<uint32_t, WSChannel*>    _wsChannels;           // List of channels by client ID
+        static std::map<std::string, WSChannel*> _wsChannelsBySession;  // List of channels by session cookie string
+        //static std::list<WSChannel*>             _wsChannelsList;       // List of all channels
+        static AsyncWebSocket* _server;
 
         static WSChannel* _lastWSChannel;
-        static WSChannel* getWSChannel(int pageid);
         static WSChannel* getWSChannel(std::string session);
 
     public:
