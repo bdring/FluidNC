9850a29ad07af41cf03eaf78492267552779af51
bdring <barton.dring@gmail.com>
2025-12-09 13:11:38 -0600

    Reference littlefs.bin in the wifi folder for posix, like win does.

--- install_scripts/posix/install-fs_s3.sh
+++ install_scripts/posix/install-fs_s3.sh
@@ -6,7 +6,7 @@ if ! . ./tools.sh; then exit 1; fi
 
 export chip="esp32s3"
 
-LocalFS="0x3d0000 ${BuildType}/littlefs.bin"
+LocalFS="0x3d0000 wifi/littlefs.bin"
 esptool_write $LocalFS
 
 deactivate
