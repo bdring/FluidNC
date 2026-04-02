98031f5301cb5b3971af8728eb292ce4de883920
Lyle Lohman <lyle.lohman@gmail.com>
2026-04-02 15:52:34 -0700

    Fix host SPI and I2S capture split

--- FluidNC/capture/Stage1HostSupport.cpp
+++ FluidNC/capture/Stage1HostSupport.cpp
@@ -72,23 +72,3 @@ int i2c_read(objnum_t bus_number, uint8_t address, uint8_t* data, size_t count) 
     }
     return Stage1HostSupport::g_i2c.readResult;
 }
-
-bool spi_init_bus(pinnum_t sck_pin, pinnum_t miso_pin, pinnum_t mosi_pin, bool dma, int8_t sck_drive_strength, int8_t mosi_drive_strength) {
-    ++Stage1HostSupport::g_spi.initCalls;
-    Stage1HostSupport::g_spi.sck = sck_pin;
-    Stage1HostSupport::g_spi.miso = miso_pin;
-    Stage1HostSupport::g_spi.mosi = mosi_pin;
-    Stage1HostSupport::g_spi.dma = dma;
-    Stage1HostSupport::g_spi.sckDrive = sck_drive_strength;
-    Stage1HostSupport::g_spi.mosiDrive = mosi_drive_strength;
-    return Stage1HostSupport::g_spi.initResult;
-}
-
-void spi_deinit_bus() {
-    ++Stage1HostSupport::g_spi.deinitCalls;
-}
-
-extern "C" void i2s_out_init(i2s_out_init_t* init_param) {
-    Stage1HostSupport::g_i2so.called = true;
-    Stage1HostSupport::g_i2so.params = *init_param;
-}
--- FluidNC/capture/i2s_out.cpp
+++ FluidNC/capture/i2s_out.cpp
@@ -1,10 +1,14 @@
 #include "Driver/i2s_out.h"
+
+#include "Stage1HostSupport.h"
+
 uint8_t g_i2soLevels[I2S_OUT_NUM_BITS] {};
 int     g_i2soWriteCalls[I2S_OUT_NUM_BITS] {};
 int     g_i2soDelayCalls = 0;
 
 void i2s_out_init(i2s_out_init_t* params) {
-    return;
+    Stage1HostSupport::g_i2so.called = true;
+    Stage1HostSupport::g_i2so.params = *params;
 }
 uint8_t i2s_out_read(pinnum_t pin) {
     return g_i2soLevels[pin];
