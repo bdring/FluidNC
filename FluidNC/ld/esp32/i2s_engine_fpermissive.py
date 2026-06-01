e854f598f919e1d30e80bdee9f39dbc3f767c25a
Mitch Bradley <wmb@firmworks.com>
2026-06-01 12:30:01 -1000

    Fixed Windows build problem by normalizing a path in a script

--- FluidNC/ld/esp32/i2s_engine_fpermissive.py
+++ FluidNC/ld/esp32/i2s_engine_fpermissive.py
@@ -1,8 +1,11 @@
 Import("env")
+import os
 
 
 def enable_fpermissive_for_i2s_engine(env, node):
-    if not node.get_path().endswith("esp32/esp32/i2s_engine.cpp"):
+    expected_suffix = os.path.normpath("esp32/esp32/i2s_engine.cpp")
+    source_path = os.path.normpath(node.get_path())
+    if not source_path.endswith(expected_suffix):
         return node
 
     return env.Object(node, CXXFLAGS=env.get("CXXFLAGS", []) + ["-fpermissive"])
